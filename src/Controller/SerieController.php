efe536a5705266b692c5f9658e03565f5ce2c64b
Akila061 <arthaud.frederic@gmail.com>
2022-10-20 16:02:09 +0200

    Commit de fin de journée

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Serie;
+use App\Repository\SerieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,18 +11,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class SerieController extends AbstractController
 {
     #[Route('/', name: 'series_list')]
-    public function list(): Response
+    public function list(SerieRepository $serieRepository): Response
     {
-        $series = [
-            [
-                'id' => 1,
-                'title' => 'Game of thrones'
-            ],
-            [
-                 'id' => 2,
-                    'title' => 'Alias'
-            ]
-        ];
+        //Récupérer les séries dans la BDD :
+//        $series = $serieRepository->findAll();
+        $series = $serieRepository->findBy(array(),array('firstAirDate' => 'DESC', 'name'=> 'ASC'));
         return $this->render('serie/index.html.twig',[
             'series' => $series
         ]);
@@ -28,13 +23,25 @@ class SerieController extends AbstractController
 
     //Route/series/1 series_details details.html.twig
     #[Route('/{id}',  name: 'series_details', requirements: ['id' => '\d+'],)]
-    public function detail(int $id): Response
+    public function detail(int $id, SerieRepository $serieRepository): Response
     {
         //TODO: Récupérer la série à afficher en BDD
+        $details = $serieRepository->find($id);
+
         return $this->render('serie/details.html.twig',[
-            'id' => $id
+            'id' => $id,
+            'details' => $details
         ]);
+
+        // plus rapide :
+//        #[Route('/{id}', name: 'series_detail', requirements: ['id' => '\d+'])]
+//        public function detail(Serie $serie): Response
+//    {
+//        return $this->render('serie/details.html.twig', [
+//            'details' => $serie
+//        ]);
     }
+
 
 
 
