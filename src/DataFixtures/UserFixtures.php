b3aa1ce0c249cf61120862037058433d7cc795dc
Akila061 <arthaud.frederic@gmail.com>
2022-10-26 14:35:47 +0200

    Mise en place de docker (pour générer l'envoie de mail)

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    public function __construct(private UserPasswordHasherInterface $hasher){
+
+    }
+    public function load(ObjectManager $manager): void
+    {
+       $admin = new User();
+       $admin->setEmail('admin@email.com');
+       $admin->setPassword($this->hasher->hashPassword($admin, 'admin'));
+       $admin->setRoles(['ROLE_ADMIN']);
+       $admin->setFirstName('admin');
+       $admin->setLastName('admin');
+       $manager->persist($admin);
+
+        $john= new User();
+        $john->setEmail('john@gmail.com');
+        $john->setPassword($this->hasher->hashPassword($john, 'john'));
+        $john->setFirstName('john');
+        $john->setLastName('john');
+        $manager->persist($john);
+
+
+        $manager->flush();
+    }
+
+
+
+}
